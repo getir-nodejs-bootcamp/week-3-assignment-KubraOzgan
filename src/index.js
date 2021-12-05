367d1b41363327c0259d135310989c3d15cfeef5
Kübra Özgan <86002359+KubraOzgan@users.noreply.github.com>
2021-12-05 18:02:08 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,4 +10,8 @@ app.get('/', (req, res) => {
 const toDoRouter = require('./router/todos');
 app.use('/todos', toDoRouter);
 
+app.get('*', (req, res) => { 
+    res.status(404).send('PAGE NOT FOUND!'); 
+});
+
 app.listen(3000);
